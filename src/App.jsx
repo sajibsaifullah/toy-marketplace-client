5d9f0b6b50936240894ce5558d6f258c95cc6d46
sajibsaifullah <dusajibphy@gmail.com>
2023-05-17 21:28:28 +0600

    project initialised

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl text-purple-600 text-center font-bold mt-12">
+        Hello From Toy MarketPlace
+      </h1>
+      <div className="text-center mt-4">
+        <button className="btn btn-primary">Button</button>
+        <button className="btn btn-secondary">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
