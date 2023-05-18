8f52816f3ffb0c3047dcc3c8888d67ab7c7ea18c
sajibsaifullah <dusajibphy@gmail.com>
2023-05-18 18:53:37 +0600

    layout, header & footer component added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import MainLayout from "../layout/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <MainLayout></MainLayout>,
   },
 ]);
 
