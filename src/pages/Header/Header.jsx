ff6a801982cf22abb102aed6fc63bc56e71c341c
sajibsaifullah <dusajibphy@gmail.com>
2023-05-18 19:25:45 +0600

    navbar added

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,11 +1,61 @@
-
-
 const Header = () => {
-    return (
-        <div>
-            <h2 className="text-5xl">this is Header</h2>
+  return (
+    <div className="container mx-auto">
+      <div className="navbar bg-base-200">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">ToyHouse</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://shorturl.at/kT129" />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
