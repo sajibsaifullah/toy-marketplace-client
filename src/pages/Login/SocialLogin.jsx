8041358978e546fa0a6f9ecb31c2a43642018c49
sajibsaifullah <dusajibphy@gmail.com>
2023-05-21 10:07:19 +0600

    redirect home after login

--- src/pages/Login/SocialLogin.jsx
+++ src/pages/Login/SocialLogin.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 import { FaGoogle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
   const { googleSignIn } = useContext(AuthContext);
+  const navigate = useNavigate();
   const handleGoogleSignIn = () => {
     googleSignIn()
       .then((result) => {
         console.log(result.user);
+        navigate('/');
       })
       .catch((error) => console.log(error));
   };
